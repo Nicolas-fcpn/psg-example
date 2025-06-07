f2ec1c97fe9920775b8f767da98e10de89d00140
Nicolas Molina <nmolinag@fcpn.edu.bo>
2025-06-06 21:18:44 -0400

    Resta de tres números

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,5 @@
+entero_1 = int(input("Ingrese el primer valor entero: "))
+entero_2 = int(input("Ingrese el segundo valor entero: "))
+entero_3 = int(input("Ingrese el segundo valor entero: "))
+resultado = entero_1 - entero_2 - entero_3
+print(f"El resultado de la resta es: {resultado}")
