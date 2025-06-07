d4778ef577cd77eaa92553ce43e4d168acfe3112
Nicolas Molina <nmolinag@fcpn.edu.bo>
2025-06-07 01:48:15 -0400

    Cadena invertida

---
+++ retos_sesion_07/ejercicio_04.py
@@ -0,0 +1,7 @@
+# Invierte la siguiente palabra "electroencefalografista"
+
+cadena_o = "electroencefalografista"
+longitud_c = len(cadena_o)
+cadena_i = cadena_o[-1:-longitud_c-1:-1]
+print(f"Cadena invertida: {cadena_i}")
+
