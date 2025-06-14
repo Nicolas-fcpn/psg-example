c2bff26c37adf8dc2e45aee8cdcfce1935bc0e43
Nicolas Molina <nmolinag@fcpn.edu.bo>
2025-06-14 08:50:48 -0400

    Guía telefónica

---
+++ retos_sesion_08/ejercicio_05.py
@@ -0,0 +1,8 @@
+# De la siguiente guía telefónica genera una tupla anidada e imprime el resultado
+
+tupla_a = (('Jhon', 591123456, 'Villa fátima'),
+           ('Jane', 591546324, 'Obrajes'),
+           ('Dogg', 591222421, 'Miraflores'),
+           ('Kity', 591753159, 'Achumani'))
+
+print(tupla_a)
