3d66658e440c36524d26c81f721c2ccb14220164
Nicolas Molina <nmolinag@fcpn.edu.bo>
2025-06-24 00:11:09 +0000

    Print sin formato

--- retos_sesion_05/ejercicio_02.py
+++ retos_sesion_05/ejercicio_02.py
@@ -1,10 +1,13 @@
 # Convertir Fahrenheit a Centígrados
 temp_1_f = 25
 temp_1_c = 5 * (temp_1_f - 32) / 9
-print(f"{temp_1_f} °F es {temp_1_c} °C")
+print(temp_1_f, "°F equivale a:")
+print(temp_1_c, "°C")
 temp_2_f = 300
 temp_2_c = 5 * (temp_2_f - 32) / 9
-print(f"{temp_2_f} °F es {temp_2_c} °C")
+print(temp_2_f, "°F equivale a:")
+print(temp_2_c, "°C")
 temp_3_f = 76
 temp_3_c = 5 * (temp_3_f - 32) / 9
-print(f"{temp_3_f} °F es {temp_3_c} °C")
+print(temp_3_f, "°F equivale a:")
+print(temp_3_c, "°C")
