ab2b344ea69cfa952057362343c9759cff56d614
Nicolas Molina <nmolinag@fcpn.edu.bo>
2025-06-28 01:39:03 -0400

    Lista con números

---
+++ retos_sesion_09/ejercicio_01.py
@@ -0,0 +1,14 @@
+# Crear una lista con 5 números flotantes y 5 enteros y
+# mostrar todos los elementos y sus tipos de datos utilizando sus índices
+
+lista = [1.5, 3.14, 2.19, 0.999, 100.0001, 1000, 23, 42, 2025, 6]
+print(lista[0], type(lista[0]))
+print(lista[1], type(lista[1]))
+print(lista[2], type(lista[2]))
+print(lista[3], type(lista[3]))
+print(lista[4], type(lista[4]))
+print(lista[5], type(lista[5]))
+print(lista[6], type(lista[6]))
+print(lista[7], type(lista[7]))
+print(lista[8], type(lista[8]))
+print(lista[9], type(lista[9]))
