76156a8df4c1db82814df8919eabc1ca5b975d00
Nicolas Molina <nmolinag@fcpn.edu.bo>
2025-07-12 03:13:34 -0400

    Animal marino

---
+++ retos_sesion_11/ejercicio_01.py
@@ -0,0 +1,9 @@
+# Utiliza un diccionario para almacenar información de un animal marino de un acuario, 
+# registra información como especie, habitat, dieta, estado de salud, edad y en un set 
+# los nombre de los responsables de su cuidado
+
+animal_marino = {
+    "especie": "Tortuga marina", "habitat": "Océano Pacífico",
+    "dieta": "Algas marinas", "estado_salud": "Saludable",
+    "edad": 45, "responsables": {"Juan", "María", "Carlos"}}
+print("Información del animal marino:", animal_marino)
